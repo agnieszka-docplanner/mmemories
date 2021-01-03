85412c19063c8a08abb1c109e5a7359b1482d9de
Agnieszka <agnieszka.pietruczuk@docplanner.com>
2021-01-03 01:19:27 +0100

    Shared libraries

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,15 @@
-use std::alloc::{GlobalAlloc, Layout, System};
-use std::sync::atomic::{Ordering::SeqCst, AtomicUsize};
+extern crate findshlibs;
 
-/*struct NullAllocator;
-
-unsafe impl GlobalAlloc for NullAllocator {
-    unsafe fn alloc(&self, _lt: Layout) -> *mut u8 {
-        panic!("you wish!");
-        std::ptr::null_mut()
-    }
-    unsafe fn dealloc(&self, ptr: *mut u8, _lt: Layout) {
-        panic!("never allocated");
-    }
-}
-*/
-static ALLOCATED: AtomicUsize = AtomicUsize::new(0);
-
-struct Counter;
-unsafe impl GlobalAlloc for Counter {
-    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        let ret = System.alloc(layout);
-        if !ret.is_null()
-        {
-            ALLOCATED.fetch_add(layout.size(), SeqCst);
-        }
-
-        return ret;
-    }
-
-    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
-        System.dealloc(ptr, layout);
-        ALLOCATED.fetch_sub(layout.size(), SeqCst);
-    }
-}
-
-#[global_allocator]
-static A: Counter = Counter;
+use findshlibs::{Segment, SharedLibrary, TargetSharedLibrary};
 
 fn main() {
-    println!("Allocated bytes before main: {}", ALLOCATED.load(SeqCst));
+    TargetSharedLibrary::each(|shlib| {
+        println!("{}", shlib.name().to_string_lossy());
+
+        for seg in shlib.segments() {
+            println!(" {}: segment {}",
+                seg.actual_virtual_memory_address(shlib),
+                seg.name());
+        }
+    });
 }
